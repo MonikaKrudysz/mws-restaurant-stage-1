a12522a5061ee4f5ab0ce3fbbaa3e852e517887a
Monika Krudysz <krudyszmonika@gmail.com>
2018-06-01 22:36:34 +0200

    chore: add gulp responsive package
    Add gulp-responsive package and add new gulp task related to images.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
+var responsive = require('gulp-responsive');
 var browserSync = require('browser-sync').create();
 
 gulp.task('watch', function () {
@@ -11,6 +12,39 @@ gulp.task('watch', function () {
     files.forEach((file) => {
         gulp.watch(file).on('change', browserSync.reload);
     });
+});
+
+gulp.task('images', function () {
+    return gulp.src('img/*.jpg')
+        .pipe(responsive({
+            '*.jpg': [
+                {
+                    width: 320,
+                    rename: {suffix: '-320'}
+                },
+                {
+                    width: 480,
+                    rename: {suffix: '-480'}
+                },
+                {
+                    width: 600,
+                    rename: {suffix: '-600'}
+                },
+                {
+                    width: 760,
+                    rename: {suffix: '-760'}
+                },
+                {
+                    width: 800,
+                    rename: {suffix: '-800'}
+                }
+            ],
+        }, {
+            quality: 70,
+            progressive: true,
+            withMetadata: false,
+        }))
+        .pipe(gulp.dest('responsive_images'))
 });
 
 gulp.task('default', []);
